35fcbfdb1c5cbb5fdc24714acde0538a276afade
Muhammad Kabeer <mknadeem721@gmail.com>
2025-12-20 03:13:02 +0500

    Fix navbar height, scroll indicator positioning, and SSR compatibility

--- frontend/src/app/components/layout/header/header.component.ts
+++ frontend/src/app/components/layout/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,24 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isScrolled = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isScrolled = window.pageYOffset > 50;
+    }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.onWindowScroll();
+    }
+  }
+
   toggleMobileMenu() {
     // Mobile menu toggle logic
   }
